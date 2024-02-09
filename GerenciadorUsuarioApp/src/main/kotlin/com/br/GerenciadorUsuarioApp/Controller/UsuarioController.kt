2887df13977b3d4a7f90232d8b2f7d1f95e0695f
Gabriel Rhoden <gabrielrhodden@gmail.com>
2024-02-09 17:15:55 -0300

    adicionando controllers

--- GerenciadorUsuarioApp/src/main/kotlin/com/br/GerenciadorUsuarioApp/Controller/UsuarioController.kt
+++ GerenciadorUsuarioApp/src/main/kotlin/com/br/GerenciadorUsuarioApp/Controller/UsuarioController.kt
@@ -2,24 +2,10 @@ package com.br.GerenciadorUsuarioApp.Controller
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 
-
 @Controller
 class UsuarioController {
-
     @GetMapping("formulario-cadastro")
     fun abrirFormularioCadastro() : String{
         return "formulario-cadastro"
     }
-
-    @GetMapping("testes")
-    fun teste() : String{
-        return "teste"
-    }
-
-    @GetMapping("masters")
-    fun master() : String{
-        return "master"
-    }
-
-
 }
